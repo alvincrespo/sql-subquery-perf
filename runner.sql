8a5c5866c1de24e373ede8d5a2cd4fba1d58e920
Alvin Crespo <alvin.crespo@gmail.com>
2024-07-13 10:20:33 -0400

    fixing formatting

--- runner.sql
+++ runner.sql
@@ -1,41 +1,52 @@
 SELECT
-	*
+  *
 FROM
-	books
+  books
 WHERE
-	author_id NOT IN(
-		SELECT
-			author_id FROM authors
-		WHERE
-			nationality != "Georgia");
-
-SELECT books.*
-FROM books
-LEFT JOIN (
-    SELECT author_id
-    FROM authors
-    WHERE nationality != 'Georgia'
-) AS filtered_authors ON books.author_id = filtered_authors.author_id
-WHERE filtered_authors.author_id IS NULL;
+  author_id NOT IN(
+    SELECT
+      author_id
+    FROM
+      authors
+    WHERE
+      nationality != "Georgia"
+  );
 
 SELECT
-	*
+  books.*
 FROM
-	books
+  books
+  LEFT JOIN (
+    SELECT
+      author_id
+    FROM
+      authors
+    WHERE
+      nationality != 'Georgia'
+  ) AS filtered_authors ON books.author_id = filtered_authors.author_id
 WHERE
-	NOT EXISTS (
-		SELECT
-			1
-		FROM
-			authors
-		WHERE
-			authors.author_id = books.author_id
-			AND authors.nationality != 'Georgia');
+  filtered_authors.author_id IS NULL;
+
+SELECT
+  *
+FROM
+  books
+WHERE
+  NOT EXISTS (
+    SELECT
+      1
+    FROM
+      authors
+    WHERE
+      authors.author_id = books.author_id
+      AND authors.nationality != 'Georgia'
+  );
 
 -- Measure performance for each query
-
 CALL measure_subquery_query_performance();
+
 CALL measure_join_query_performance();
+
 CALL measure_exists_query_performance();
 
 --- Perf Metrics
